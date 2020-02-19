2fc2a949920414dd64980d2ed90956386d3f6ca0
OrlaBr <orlabreslin@yahoo.com>
2020-02-19 14:22:28 +0000

    Added env and gitifnore files

--- app.py
+++ app.py
@@ -1,7 +1,16 @@
 import os
 from flask import Flask
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+
 
 app = Flask(__name__)
+
+app.config["MONGO_DBNAME"] = "task_manager"
+app.config["MONGO_URI"] = os.environ.get('env.py')
+
+
+mongo = PyMongo(app)
 
 
 @app.route('/')
